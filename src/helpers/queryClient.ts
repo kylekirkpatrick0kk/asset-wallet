dae5c1ef30ba94c202687aa513298fbedac93b36
Kyle Kirkpatrick <kylekirkpatrick0kk@gmail.com>
2025-02-15 23:36:04 -0700

    create tanstack query client and initialize the query for vue

---
+++ src/helpers/queryClient.ts
@@ -0,0 +1,6 @@
+// filepath: /src/queryClient.js
+import { QueryClient } from '@tanstack/vue-query';
+
+const queryClient = new QueryClient();
+
+export default queryClient;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 import './style.css'
 import App from './App.vue'
+import queryClient from './helpers/queryClient'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(VueQueryPlugin, { queryClient });
+
+app.mount('#app');
